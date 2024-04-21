8102f5cbe8dab07df6b8356fcdf78808307cf29c
MdJusef <mdjusef143@gmail.com>
2024-04-21 18:09:37 +0600

    category,product,model

---
+++ app/Models/Sub_Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Sub_Category extends Model
+{
+    use HasFactory;
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function product():HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+
+// -----------------Category --------------------
+Route::post('add-category', [CategoryController::class, 'addCategory']);
+Route::post('update-category', [CategoryController::class, 'updateCategory']);
+Route::get('delete-category', [CategoryController::class, 'deleteCategory']);
+Route::get('show-category', [CategoryController::class, 'showCategory']);
+
+
